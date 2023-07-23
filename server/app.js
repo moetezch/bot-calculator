8d58fbd57b5882061562494c07af359becc01bf9
moetezch <contact@moetez.me>
2023-07-23 22:22:13 +0200

    refactor: change prettier config

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-import express from 'express'
-import http from 'http'
+import express from 'express';
+import http from 'http';
+import('./database/index.js');
 
-const app = express()
-const server = http.createServer(app)
-const port = 3000
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+    console.log(`Server is running on http://localhost:${port}`);
+});
