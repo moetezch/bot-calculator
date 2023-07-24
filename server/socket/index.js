2936a89688e69dbcda374678421c86c1d334cd39
moetezch <contact@moetez.me>
2023-07-24 22:31:58 +0200

    feat: implement socket io events in backend

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,17 +1,35 @@
 import { Server } from 'socket.io';
+import { evaluate } from 'mathjs';
 
+const invalidCommand = 'Invalid command';
 export const initSocket = (server) => {
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            origin: 'http://localhost:3000',
+        },
+    });
     io.on('connection', (socket) => {
+        const history = [];
         console.log('a user connected');
         socket.on('disconnect', () => {
             console.log('user disconnected');
         });
         socket.on('history', () => {
-            // return history
+            socket.emit('history', history);
         });
         socket.on('calculate', (command) => {
-            // calculate then emit the result
+            const entry = {
+                command,
+            };
+            try {
+                const result = evaluate(command);
+                entry.result = result;
+                socket.emit('result', result);
+            } catch (error) {
+                entry.result = invalidCommand;
+                socket.emit('result', invalidCommand);
+            }
+            history.push(entry);
         });
     });
 };
